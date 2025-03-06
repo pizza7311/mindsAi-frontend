2d83e6f25fbb203ec18dd8400f93c095e2df8bf1
pizza7311 <51106108+pizza7311@users.noreply.github.com>
2025-03-06 22:48:13 +0900

    style: Add custom scroll style

--- src/app/_components/TodoList.tsx
+++ src/app/_components/TodoList.tsx
@@ -11,7 +11,16 @@ const TodoList = () => {
       <div className="border-b border-gray-200">
         <h1 className="text-4xl font-extrabold p-4">Today&apos;s work</h1>
       </div>
-      <div className="w-full pt-2">
+      {/* custom scroll style from : https://preline.co/docs/custom-scrollbar.html */}
+      <div
+        className="w-full pt-2 overflow-y-scroll max-h-[800px]
+      [&::-webkit-scrollbar]:w-2
+  [&::-webkit-scrollbar-track]:bg-gray-100
+  [&::-webkit-scrollbar-thumb]:bg-gray-300
+  dark:[&::-webkit-scrollbar-track]:bg-neutral-700
+  dark:[&::-webkit-scrollbar-thumb]:bg-neutral-500
+      "
+      >
         {todoList?.length === 0 ? (
           <TodoListPlaceHolder></TodoListPlaceHolder>
         ) : (
